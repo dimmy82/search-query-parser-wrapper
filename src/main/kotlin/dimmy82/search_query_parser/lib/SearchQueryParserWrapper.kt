5248bfc2a21ea848061b3541d15ad2b1f0bfaa63
teki.sei <teki.sei@uzabase.com>
2023-09-28 23:53:04 +0900

    change 「System.loadLibrary」 to 「System.load」

--- src/main/kotlin/dimmy82/search_query_parser/lib/SearchQueryParserWrapper.kt
+++ src/main/kotlin/dimmy82/search_query_parser/lib/SearchQueryParserWrapper.kt
@@ -4,60 +4,52 @@ package dimmy82.search_query_parser.lib
 
 import dimmy82.search_query_parser.domain.ICondition
 import java.io.File
-import java.lang.invoke.MethodHandles
-import java.lang.reflect.Field
-import java.lang.reflect.Modifier
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
-import java.util.logging.Level
 import java.util.logging.Logger
+import kotlin.io.path.absolutePathString
 import parseQueryToCondition as parseQueryToConditionJson
-
-// append these jvm options for execution
-// --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/jdk.internal.loader=ALL-UNNAMED
 
 /**
  * search-query-parser is made to parse complex search query into layered search conditions,
  * so it will be easy to construct Elasticsearch query DSL or something else.
  * this is a wrapper of the Rust library (https://crates.io/crates/search-query-parser).
  * Requirement:
+ * - OS: Linux or Mac(arm64)
  * - Java 17
- * - Linux or Mac(arm64)
  */
 class SearchQueryParserWrapper private constructor() {
     companion object {
+        private const val VERSION = "1.1.42"
+        private const val NATIVE_VERSION = "0.1.4"
+        private const val NATIVE_LIBRARY_NAME = "search_query_parser_${NATIVE_VERSION}"
+        private const val NATIVE_LIBRARY_FILE_IN_MAC = "lib${NATIVE_LIBRARY_NAME}.dylib"
+        private const val NATIVE_LIBRARY_FILE_IN_LINUX = "lib${NATIVE_LIBRARY_NAME}.so"
+        private val nativeLibraryFilePath: String
         private val logger = Logger.getLogger("SearchQueryParser")
-        private const val NATIVE_LIBRARY_NAME = "search_query_parser_0.1.4"
-        private val nativeLibraryFiles = setOf(
-            "lib${NATIVE_LIBRARY_NAME}.so",
-            "lib${NATIVE_LIBRARY_NAME}.dylib"
-        )
 
         init {
-            val nativeLibraryPath = exportNativeLibrary()
-            prepareForLoadNativeLibrary(nativeLibraryPath)
+            nativeLibraryFilePath = exportNativeLibrary()
         }
 
         private fun exportNativeLibrary(): String {
-            val osArch = osArch()
-            val nativeLibraryPath = nativeLibraryExportPath()
-            nativeLibraryFiles.forEach { nativeLibraryFile ->
-                val nativeLibraryFilePath = "$osArch${File.separator}$nativeLibraryFile"
-                logger.info("initialize => export native library file [$nativeLibraryFilePath] out of jar")
-                SearchQueryParserWrapper::class.java.getResourceAsStream(nativeLibraryFilePath)
-                    ?.let { inputStream ->
-                        Files.copy(
-                            inputStream,
-                            Paths.get(nativeLibraryPath, nativeLibraryFile),
-                            StandardCopyOption.REPLACE_EXISTING
-                        )
-                    }
-            }
-            return nativeLibraryPath
+            val nativeLibraryFile = nativeLibraryFile()
+            val nativeLibraryFileInput = "${arch()}${File.separator}$nativeLibraryFile"
+            val nativeLibraryFileOutput = Paths.get(nativeLibraryExportPath(), nativeLibraryFile)
+            logger.info("initialize => export native library file [$nativeLibraryFileOutput] out of jar")
+            SearchQueryParserWrapper::class.java.getResourceAsStream(nativeLibraryFileInput)
+                ?.let { inputStream ->
+                    Files.copy(
+                        inputStream,
+                        nativeLibraryFileOutput,
+                        StandardCopyOption.REPLACE_EXISTING
+                    )
+                }
+            return nativeLibraryFileOutput.absolutePathString()
         }
 
-        private fun osArch() =
+        private fun arch() =
             System.getProperty("os.arch")?.let {
                 if (setOf("aarch64", "arm64").contains(it)) {
                     "arm64"
@@ -65,6 +57,17 @@ class SearchQueryParserWrapper private constructor() {
                     "x86_64"
                 }
             } ?: "x86_64"
+
+        private fun nativeLibraryFile() =
+            System.getProperty("os.name").lowercase().let { os ->
+                if (os.contains("mac")) {
+                    NATIVE_LIBRARY_FILE_IN_MAC
+                } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+                    NATIVE_LIBRARY_FILE_IN_LINUX
+                } else {
+                    throw RuntimeException("Unsupported OS:[${os}]")
+                }
+            }
 
         private fun nativeLibraryExportPath(): String {
             val m2Repository = Paths.get(
@@ -75,7 +78,7 @@ class SearchQueryParserWrapper private constructor() {
                 "github",
                 "dimmy82",
                 "search-query-parser",
-                "1.1.41"
+                VERSION
             ).toFile()
             val nativeLibrary = if (m2Repository.isDirectory) {
                 Paths.get(m2Repository.absolutePath, "native-library")
@@ -88,44 +91,12 @@ class SearchQueryParserWrapper private constructor() {
             return nativeLibrary.toFile().absolutePath
         }
 
-        private fun prepareForLoadNativeLibrary(nativeLibraryPath: String) {
-            // field modifier
-            val lookup = MethodHandles.privateLookupIn(Field::class.java, MethodHandles.lookup())
-            val modifiersField = lookup.findVarHandle(Field::class.java, "modifiers", Int::class.javaPrimitiveType)
-
-            // jdk.internal.loader.NativeLibraries$LibraryPaths in Java 17
-            val libraryPathsClass = Class.forName("jdk.internal.loader.NativeLibraries\$LibraryPaths")
-            val libraryUserPaths = libraryPathsClass.getDeclaredField("USER_PATHS").also { it.setAccessible(true) }
-
-            // remove final from NativeLibraries$LibraryPaths.USER_PATHS
-            modifiersField.set(
-                libraryUserPaths,
-                libraryUserPaths.modifiers and Modifier.FINAL.inv()
-            )
-
-            // append path to java.library.path
-            @Suppress("UNCHECKED_CAST")
-            val libraryCurrentUserPaths = libraryUserPaths.get(null) as Array<String>
-            val libraryNewUserPaths = arrayOf(nativeLibraryPath) + libraryCurrentUserPaths
-            logger.log(
-                Level.CONFIG,
-                "initialize => java.library.path: ${libraryNewUserPaths.joinToString(separator = File.pathSeparator)}"
-            )
-            libraryUserPaths.set(null, libraryNewUserPaths)
-
-            // set final back to NativeLibraries$LibraryPaths.USER_PATHS
-            modifiersField.set(
-                libraryUserPaths,
-                libraryUserPaths.modifiers and Modifier.FINAL.inv()
-            )
-        }
-
         val SEARCH_QUERY_PARSER = SearchQueryParserWrapper()
     }
 
     init {
         val start = System.currentTimeMillis()
-        System.loadLibrary(NATIVE_LIBRARY_NAME)
+        System.load(nativeLibraryFilePath)
         logger.info("initialize => native library loaded [time: ${System.currentTimeMillis() - start} ms]")
     }
 
